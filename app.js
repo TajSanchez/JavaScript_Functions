eefcb42fa0f08f7a279dbbfbdbdb5953104ba4f9
Taj Sanchez <tajmarie92@gmail.com>
2021-03-10 21:09:42 -0600

    Updated algorithms

--- app.js
+++ app.js
@@ -2,38 +2,32 @@ console.log("Hello World!\n==========\n");
 
 // Exercise 1 Section
 console.log("EXERCISE 1:\n==========\n");
-function printOdds(count){
-for (let i = 1; i <= count; i++){
-    if (i % 2 === 1){
-        console.log(i);
+
+let count = window.prompt("Give me a number to count to and I will print the odds")
+
+function printOdds(count) {
+    for (let i = 1; i <= count; i++) {
+        if (i % 2 === 1) {
+            console.log(i);
+        }
     }
-
-
-
 }
-}
-console.log(printOdds (100));
 
-
-
-
-
-
-
-
-
+console.log(printOdds(count));
 
 // Exercise 2 Section
 console.log("EXERCISE 2:\n==========\n");
-function checkAge(age=29){
-const aboveSixteen = "You can drive!"
-const belowSixteen = "Tough luck."
-if (age > 16){
-    return aboveSixteen;
-} else {
-    return belowSixteen;
 
- 
+let age = window.prompt("Give me your age!")
 
-}}
-console.log(checkAge(19));
+function checkAge(age) {
+    const aboveSixteen = "You can drive!"
+    const belowSixteen = "Tough luck."
+    if (age >= 16) {
+        return aboveSixteen;
+    } else {
+        return belowSixteen;
+    }
+}
+
+console.log(checkAge(age));
