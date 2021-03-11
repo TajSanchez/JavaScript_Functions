4241ca6053bcf2bf0407361624a683629f052f97
Taj Sanchez <tajmarie92@gmail.com>
2021-03-10 19:55:05 -0600

    Completed Exercise 1

--- app.js
+++ app.js
@@ -2,6 +2,26 @@ console.log("Hello World!\n==========\n");
 
 // Exercise 1 Section
 console.log("EXERCISE 1:\n==========\n");
+function printOdds(count){
+for (let i = 1; i <= count; i++){
+    if (i % 2 === 1){
+        console.log(i);
+    }
+
+
+
+}
+}
+console.log(printOdds (100));
+
+
+
+
+
+
+
+
+
 
 // Exercise 2 Section
 console.log("EXERCISE 2:\n==========\n");
